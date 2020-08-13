b568ebbdab0248024b2981ca964a8739d5a0e6b9
Gash <mungai.gachango@gmail.com>
2020-08-13 18:16:26 +0300

    Final first Practices on bash

--- more_examples.sh
+++ more_examples.sh
@@ -112,12 +112,133 @@
 #((result=x+y))
 #echo "X + Y = $result"
 
-######################################################
+#####################################
 #######Combine String variables#####
-string1="Linux"
-string2="Hint"
-echo "$string1 $string2"
-string3=$string1+$string2
+#string1="Linux"
+#string2="Hint"
+#echo "$string1 $string2"
+#string3=$string1+$string2
 #string3+=" is a good tutorial blog site"
-echo "$string3 is a good tutorial blog site"
+#echo "$string3 is a good tutorial blog site"
+
+
+#####################################
+#######Get substring of String #####
+#Str="Learn Linux from LinuxHint"
+#substr=${Str:6:9}
+#echo $substr
+#Here, the value, 6 indicates the starting point from where the substring will start and 5 indicates the length of the substring.
+
+
+#####################################
+#######Add Two Numbers:#####
+#echo "Enter First Number"
+#read x
+#echo "Enter Second Number"
+#read y
+#(( sum=x+y ))
+#echo "The result of addition is $sum"
+
+
+#####################################
+#######Create Function#####
+#function F1 ()
+#{
+#	echo "I like bash programming"
+#}
+
+#F1
+
+
+###########################################
+#######Create Function with parameters#####
+#Rectangle_Area()
+#{
+#area=$(( $1*$2 ))
+#echo "Area is: $area"
+#}
+#Rectangle_Area 10 20 20
+
+
+###########################################
+#######Pass Return Value from Function:####
+#function greeting()
+#{
+#	str="Hello, $name"
+#echo $str
+#}
+#echo "Enter Your name"
+#read name
+#echo "Return value of the function is $(greeting)"
+
+
+###########################
+#######Make Directory:#####
+#echo "Enter Directory name"
+#read newdir
+#`mkdir $newdir`
+
+#################################################
+#######Make directory by checking existence:#####
+#echo "Enter directory name"
+#read newdir
+#if [ -d "$newdir" ]
+#then
+#	echo "Directory Exists"
+#else
+#	`mkdir $newdir`
+#echo "Directory created"
+#fi
+
+
+#########################
+#######Read a File:#####
+#file="README.md"
+#while read line; do
+#	echo $line
+#done < $file
+
+
+###############################
+#######Append to a File:#####
+#echo "Before appending to a file"
+#cat First.sh
+
+#echo -n "Learning Bash Scripting">>First.sh
+#echo "After appending the file"
+#cat First.sh
+
+###############################
+#######Test if file exists#####
+#filename=$1
+#if [ -f "$filename" ]; then
+#	echo "File exists"
+#else
+#	echo "File does not exist"
+#fi
+
+
+###########################
+#######Send Email##########
+#Recipient=mungai.gachango@gmail.com
+#Subject="Test Email from bash"
+#Message="This is a wonderful experiment"
+#`mail -s $Subject $Recipient <<< $Message`
+
+
+#########################################
+#######Get Parse Current Date:##########
+Year=`date +%Y`
+Month=`date +%m`
+Day=`date +%d`
+Hour=`date +%H`
+Minute=`date +%M`
+Second=`date +%S`
+echo `date`
+echo "Current Date is: $Day-$Month-$Year"
+echo "Current Time is: $Hour:$Minute:$Second"
+
+
+
+
 
